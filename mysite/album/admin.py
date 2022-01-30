a9bea0de56ea0f6cad18c915c5e0636cfe797a05
Vitaliy Sekach <50178320+SekachVitaliy@users.noreply.github.com>
2022-01-31 00:55:29 +0200

    create models

---
+++ mysite/album/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Category, Photo
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['description', 'category', 'published']
+    list_display_links = ['description', 'category']
+    search_fields = ['published']
+
+
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Category)
---
+++ mysite/album/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Photo(models.Model):
+    description = models.CharField(max_length=100, null=False, verbose_name='Описание')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    image = models.ImageField(null=False, blank=False, verbose_name='Фотография')
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        verbose_name_plural = 'Фотографии'
+        verbose_name = 'Фотография'
+        ordering = ['-published']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, null=False,  db_index=True, verbose_name='Название')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Категории'
+        verbose_name = 'Категория'
+        ordering = ['name']
